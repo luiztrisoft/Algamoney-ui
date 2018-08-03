bc486f065d28703d3fbd26fdc557a34eec1eacec
Luiz <luiz.trisoft@gmail.com>
2018-08-02 21:36:22 -0300

    Adição de pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
-
-import { ButtonModule} from 'primeng/button';
-import { InputTextModule } from 'primeng/inputtext';
-import {TableModule} from 'primeng/table';
-import {TooltipModule} from 'primeng/tooltip';
 import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { NavbarComponent } from './navbar/navbar.component';
 import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.component';
 
-// const rotas: Routes = [
-//   { path: '', component: AppComponent },
-//   { path: 'pagina', component: AppComponent },
-//   { path: 'pagina-com-parametro/:id', component: AppComponent }
-// ];
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { TableModule } from 'primeng/table';
+import { TooltipModule } from 'primeng/tooltip';
+import { NavbarComponent } from './navbar/navbar.component';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -30,8 +25,6 @@ import { PessoaPesquisaComponent } from './pessoa-pesquisa/pessoa-pesquisa.compo
     InputTextModule,
     TableModule,
     TooltipModule
-    // RouterModule.forRoot(rotas)
-    //causou falha ao adicionar componentes do primeng sem rotas configuradas
   ],
   providers: [],
   bootstrap: [AppComponent]
